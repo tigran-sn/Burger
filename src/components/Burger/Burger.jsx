b0522d9941faf4373c117acb546710afaf7392f8
Tigran <soghomonyantigran@gmail.com>
2019-09-06 23:38:24 +0400

    transformedIngredients (ingrediate from state) logic  in Burger.jsx

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import BurgerIngredients from "./BurgerIngredient";
 
 import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient";
 
-const Burger = props => {
+const Burger = ({ ingredients }) => {
+  const transformedIngredients = Object.keys(ingredients).map(igKey => {
+    return [...Array(ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+  // bread-top and bread-bottom ingredient are always available
   return (
     <div className={classes.Burger}>
-      {props.ingredients.meat}
       <BurgerIngredients type="bread-top" />
-      <BurgerIngredients type="cheese" />
-      <BurgerIngredients type="meat" />
-      <BurgerIngredients type="salad" />
-      <BurgerIngredients type="bacon" />
+      {transformedIngredients}
       <BurgerIngredients type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -4,7 +4,12 @@ import Burger from "../../components/Burger";
 
 class BurgerBuilder extends Component {
   state = {
-    ingredients: {},
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      cheese: 2,
+      meat: 2
+    },
     purchased: false,
     totalPrice: 0
   };

845878ad010ecf322b88f273d747f3b597e64e8c
Tigran <soghomonyantigran@gmail.com>
2019-09-07 14:28:22 +0400

    Backdrop

--- src/components/UI/Backdrop/Backdrop.jsx
+++ src/components/UI/Backdrop/Backdrop.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Aux from "../../hoc/Aux";
 import classes from "./Backdrop.module.css";
 
-const Backdrop = props => (
-  <Aux>
-    <div>Toolbar, SideDrower, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+const Backdrop = props =>
+  props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked}></div>
+  ) : null;
 
 export default Backdrop;
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -76,6 +76,11 @@ class BurgerBuilder extends Component {
       purchasing: true
     });
   };
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false
+    });
+  };
   render() {
     const disableInfo = {
       ...this.state.ingredients
@@ -85,7 +90,10 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
